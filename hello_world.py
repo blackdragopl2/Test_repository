c7d2eb14f5d8e067725395628d03aa17b6580d5c
blackdragopl2 <83238456+blackdragopl2@users.noreply.github.com>
2022-12-03 03:33:52 +0200

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,8 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 
+=======
+>>>>>>> parent of 747c817 (Revert "Update hello_world.py")
 =======
 >>>>>>> parent of 747c817 (Revert "Update hello_world.py")
 print("Hello world!")
